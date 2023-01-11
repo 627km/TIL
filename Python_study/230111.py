d6c0564f606b5ee4f7d2b0557a1e34e764334ad4
627km <rudals_jeon@naver.com>
2023-01-11 20:45:38 +0900

    feat: Add Comprehension
    - Add Fizzbuzz used Comprehension

--- Python_study/230111.py
+++ Python_study/230111.py
@@ -26,3 +26,17 @@ binet(35)
 e_time = time()
 fibo_time = e_time - s_time
 print(fibo_time)
+
+# Fizzbuzz use comprehension
+## fizzbuzz list
+num_range = [i for i in range(1,30+1)]
+
+## List Comprehension
+fb_list = []
+fb_list = [
+    'fizzbuzz' if i%15==0 else
+    'fizz' if i%3==0 else
+    'buzz' if i%5==0 else
+    i for i in num_range
+    ]
+fb_list
